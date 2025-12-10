5422c255df599cc7fbb8955e09234c8238b211e6
Łukasz Drukała <masflam@masflam.com>
2025-12-10 11:18:52 +0100

    Command line arguments

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
-int main() {
-  int n;
-  std::cin >> n;
+#include <cstdlib>
+int main(int argc, char **argv) {
+  int n = atoi(argv[1]);
 
   for (int i = 1; i <= n; ++i) {
     if (i % 3 == 0 && i % 5 == 0) {
